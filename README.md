c1b6a69f3fc78a24a8d80757cc4f4966ab2e4b4a
user <user@xcat-rocky.ch1.csquare.run>
2022-01-26 19:17:10 +0100

    use /xcatdata volume

---
+++ README.md
@@ -0,0 +1,4 @@
+# xCAT Server Container
+## Description
+xCAT is Extreme Cloud Administration Toolkit, xCAT offers complete management for bare-metal based cluster.
+
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ sudo docker run -d \
      --hostname xcatmn \
      --privileged   \
      -v /sys/fs/cgroup:/sys/fs/cgroup:ro  \
-     -v /xcatdata2:/xcatdata     \
+     -v /xcatdata:/xcatdata     \
      -v /var/log/xcat:/var/log/xcat  \
      -v /customer_data:/customer_data   \
      xcat:rocky8.4
